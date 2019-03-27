fa7d8d24eb7ba9771c2a9e4337d2a59e67142598
Nils Robert Leganger <nrl@ntnu.no>
2019-03-27 19:41:42 +0100

    Oppstaering av filstruktur

---
+++ assykkelutleie/View/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        'Dim username As String = "Administrator"
+        'Dim password = Encryption.HashString("Tastatur123")
+        'Dim salt = Encryption.GenerateSalt
+        'Dim hashedAndSalted = Encryption.HashString(String.Format("{0}{1}", password, salt))
+
+        'Console.WriteLine(password)
+        'Console.WriteLine(salt)
+        'Console.WriteLine(hashedAndSalted)
+
+
+        ' DbManager.insertNewUser(username, hashedAndSalted, salt)
+        'DbManager.Login(username, "Tastatur123")
+
+    End Sub
+End Class
