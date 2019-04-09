518f106615fde3f7b0dd01cceb9ef33f91cc96d9
aadnehelmersen <48697524+aadnehelmersen@users.noreply.github.com>
2019-04-09 21:41:26 +0200

    Add files via upload
    Funksjon som oppdaterer kunderegister

---
+++ assykkelutleie/Controller/CustomerRefresh.vb
@@ -0,0 +1,37 @@
+﻿
+Imports MySql.Data.MySqlClient
+
+Public Class CustomerRefresh
+    Private Shared connectionstring As String = "Server=mysql-ait.stud.idi.ntnu.no;Database=nilsrle;Uid=nilsrle;Pwd=TnAzsu4O;"
+
+
+    Public Shared Sub refreshCustomers()
+
+        Using sqlconnection As New MySqlConnection(connectionstring)
+
+            Dim CustomerID As Integer
+            Dim phone As Integer
+            Dim firstname As String
+            Dim surname As String
+            Dim email As String
+
+            Dim sql As New MySqlCommand("SELECT * FROM Customer ORDER BY CustomerID", sqlconnection)
+            Dim da As New MySqlDataAdapter
+            Dim table As New DataTable
+            da.SelectCommand = sql
+            da.Fill(table)
+
+            Form1.ListBox1.Items.Clear()
+            For Each row In table.Rows
+                CustomerID = row("CustomerID")
+                firstname = row("FirstName")
+                surname = row("Surname")
+                email = row("Email")
+                phone = row("TelephoneNumber")
+                Form1.ListBox1.Items.Add(firstname & " " & surname & " " & email & " " & phone)
+
+            Next row
+        End Using
+    End Sub
+End Class
+
