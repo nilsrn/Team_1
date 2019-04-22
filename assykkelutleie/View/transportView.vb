44a8b2f30b72b8b33a3b81bbeccf5e008c287860
Silje Schirmer-MIkalsen <sischimi@gmail.com>
2019-04-22 12:26:46 +0200

    getall

--- assykkelutleie/View/transportView.vb
+++ assykkelutleie/View/transportView.vb
@@ -1,3 +1,17 @@
-﻿Public Class transportView
+﻿Imports MySql.Data.MySqlClient
+Public Class transportView
+    Private Sub PutLbRentalID(table As DataTable) 'Populates the listbox with data received from the DB. 
+        lbRentalID.DataSource = table
+        lbRentalID.DisplayMember = "RentalID"
+        lbRentalID.ValueMember = "RentalID"
+    End Sub
+    Private Function GetAllRentals() 'Returns a DataTable with all rentals.
+        Dim rental As New Rentals()
+        Dim rentalsTable As DataTable = DbManager.GetAll(rental)
+        Return rentalsTable
+    End Function
 
+    Private Sub transportView_Load(sender As Object, e As EventArgs) Handles MyBase.Load 'Populates listboxes and dropdownmenues when the form loads.
+        PutLbRentalID(GetAllRentals)
+    End Sub
 End Class
