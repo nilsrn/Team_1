9b489a5ed1ae2dffee769bc355173b5161825b26
Sven-Erik Nystad <nystad97@gmail.com>
2019-05-01 14:21:26 +0200

    Visuelt

--- assykkelutleie/View/mainView.Designer.vb
+++ assykkelutleie/View/mainView.Designer.vb
@@ -45,7 +45,7 @@ Partial Class mainView
         Me.btnCustomers.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnCustomers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnCustomers.ImageKey = "(none)"
-        Me.btnCustomers.Location = New System.Drawing.Point(9, 55)
+        Me.btnCustomers.Location = New System.Drawing.Point(9, 54)
         Me.btnCustomers.Name = "btnCustomers"
         Me.btnCustomers.Size = New System.Drawing.Size(144, 45)
         Me.btnCustomers.TabIndex = 2
@@ -60,7 +60,7 @@ Partial Class mainView
         Me.btnBicycles.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnBicycles.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnBicycles.ImageKey = "(none)"
-        Me.btnBicycles.Location = New System.Drawing.Point(9, 106)
+        Me.btnBicycles.Location = New System.Drawing.Point(9, 105)
         Me.btnBicycles.Name = "btnBicycles"
         Me.btnBicycles.Size = New System.Drawing.Size(144, 45)
         Me.btnBicycles.TabIndex = 3
@@ -90,7 +90,7 @@ Partial Class mainView
         Me.btnTransport.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnTransport.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnTransport.ImageKey = "(none)"
-        Me.btnTransport.Location = New System.Drawing.Point(9, 157)
+        Me.btnTransport.Location = New System.Drawing.Point(9, 156)
         Me.btnTransport.Name = "btnTransport"
         Me.btnTransport.Size = New System.Drawing.Size(144, 45)
         Me.btnTransport.TabIndex = 4
@@ -106,9 +106,9 @@ Partial Class mainView
         Me.btnLogout.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnLogout.ImageKey = "(none)"
-        Me.btnLogout.Location = New System.Drawing.Point(11, 407)
+        Me.btnLogout.Location = New System.Drawing.Point(9, 411)
         Me.btnLogout.Name = "btnLogout"
-        Me.btnLogout.Size = New System.Drawing.Size(144, 34)
+        Me.btnLogout.Size = New System.Drawing.Size(144, 35)
         Me.btnLogout.TabIndex = 7
         Me.btnLogout.Text = "Logg ut"
         Me.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -121,9 +121,9 @@ Partial Class mainView
         Me.btnAdmin.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnAdmin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnAdmin.ImageKey = "(none)"
-        Me.btnAdmin.Location = New System.Drawing.Point(11, 363)
+        Me.btnAdmin.Location = New System.Drawing.Point(9, 360)
         Me.btnAdmin.Name = "btnAdmin"
-        Me.btnAdmin.Size = New System.Drawing.Size(144, 38)
+        Me.btnAdmin.Size = New System.Drawing.Size(144, 45)
         Me.btnAdmin.TabIndex = 5
         Me.btnAdmin.Text = "Admin"
         Me.btnAdmin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -165,7 +165,7 @@ Partial Class mainView
         Me.btnRepair.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnRepair.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnRepair.ImageKey = "(none)"
-        Me.btnRepair.Location = New System.Drawing.Point(11, 260)
+        Me.btnRepair.Location = New System.Drawing.Point(9, 258)
         Me.btnRepair.Name = "btnRepair"
         Me.btnRepair.Size = New System.Drawing.Size(144, 45)
         Me.btnRepair.TabIndex = 10
@@ -180,7 +180,7 @@ Partial Class mainView
         Me.btnInOut.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnInOut.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnInOut.ImageKey = "(none)"
-        Me.btnInOut.Location = New System.Drawing.Point(11, 209)
+        Me.btnInOut.Location = New System.Drawing.Point(9, 207)
         Me.btnInOut.Name = "btnInOut"
         Me.btnInOut.Size = New System.Drawing.Size(144, 45)
         Me.btnInOut.TabIndex = 9
@@ -195,7 +195,7 @@ Partial Class mainView
         Me.btnStatistics.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnStatistics.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnStatistics.ImageKey = "(none)"
-        Me.btnStatistics.Location = New System.Drawing.Point(11, 311)
+        Me.btnStatistics.Location = New System.Drawing.Point(9, 309)
         Me.btnStatistics.Name = "btnStatistics"
         Me.btnStatistics.Size = New System.Drawing.Size(144, 45)
         Me.btnStatistics.TabIndex = 8
